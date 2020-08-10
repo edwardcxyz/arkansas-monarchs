248ab0662ae6ecdb3ed1cbf8a444bc01ae11b4b4
Edward <eshepard81@gmail.com>
2020-08-09 21:40:11 -0500

    Change accent color; Add publications doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,7 @@ module.exports = {
             '/threats',
             '/the-plan',
             '/habitat',
+            '/publications',
             '/events',
             '/register',
             '/join-us',
