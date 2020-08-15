43c7e9654f0a16a45e66b6ab7d5975e32622c2a3
edwardcxyz <eshepard81@gmail.com>
2020-08-15 00:00:32 -0500

    Big tweaks to hero and header text

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -3,41 +3,17 @@
     class="home"
     aria-labelledby="main-title"
   >
-    <header class="hero">
-      <img
-        v-if="data.heroImage"
-        :src="$withBase(data.heroImage)"
-        :alt="data.heroAlt || 'hero'"
-      >
+    <header class="hero"></header>
 
-      <h1
-        v-if="data.heroText !== null"
-        id="main-title"
-      >
-        {{ data.heroText || $title || 'Hello' }}
-      </h1>
+    <h1 v-if="data.heroText !== null" id="main-title">
+      {{ data.heroText || $title || 'Hello' }}
+    </h1>
 
-      <p
-        v-if="data.tagline !== null"
-        class="description"
-      >
-        {{ data.tagline || $description || '' }}
-      </p>
+    <p v-if="data.tagline !== null" class="description">
+      {{ data.tagline || $description || '' }}
+    </p>
 
-      <NavLinks/>
-
-      <!-- <p
-        v-if="data.actionText && data.actionLink"
-        class="action"
-      >
-        <NavLink
-          class="action-button"
-          :item="actionLink"
-        /> -->
-      </p>
-      
-    </header>
-
+    <NavLinks/>
     <div
       v-if="data.features && data.features.length"
       class="features"
@@ -118,39 +94,43 @@ export default {
   max-width $homePageWidth
   margin 0px auto
   display block
+  height 100%
   .hero
+    margin-top 24px
     text-align center
-    img
-      max-width: 100%
-      max-height 280px
-      display block
-      margin 3rem auto 1.5rem
-      box-shadow 1px 1px 8px #333
-      border-radius 8px
-    h1
-      font-size 3rem
-    h1, .description, .action
-      margin 1.8rem auto
-    #main-title
-      font-size 2rem
-    .description
-      max-width 35rem
-      font-size 1.2rem
-      line-height 1.3
-      color lighten($textColor, 40%)
-      margin-bottom 8px
-    .action-button
-      display inline-block
-      font-size 1.2rem
-      color #fff
-      background-color $accentColor
-      padding 0.8rem 1.6rem
-      border-radius 4px
-      transition background-color .1s ease
-      box-sizing border-box
-      border-bottom 1px solid darken($accentColor, 10%)
-      &:hover
-        background-color lighten($accentColor, 10%)
+    background-image url('/img/hero-01.png')
+    height 40vh
+    width 100%
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+  h1
+    font-size 3rem
+  h1, .description, .action
+    margin 1.8rem auto
+  #main-title
+    text-align center
+    font-size 2rem
+  .description
+    max-width 35rem
+    font-size 1.2rem
+    line-height 1.3
+    color lighten($textColor, 40%)
+    margin-bottom 8px
+    text-align center
+  .action-button
+    display inline-block
+    font-size 1.2rem
+    color #fff
+    background-color $accentColor
+    padding 0.8rem 1.6rem
+    border-radius 4px
+    transition background-color .1s ease
+    box-sizing border-box
+    border-bottom 1px solid darken($accentColor, 10%)
+    &:hover
+      background-color lighten($accentColor, 10%)
   .feature-row
     display flex
     flex-flow row wrap
--- docs/.vuepress/theme/components/NavLinks.vue
+++ docs/.vuepress/theme/components/NavLinks.vue
@@ -125,6 +125,8 @@ export default {
 <style lang="stylus">
 .nav-links
   display inline-block
+  text-align center
+  width 100%
   a
     line-height 1.4rem
     color inherit
@@ -133,8 +135,17 @@ export default {
   .nav-item
     position relative
     display inline-block
-    margin-left 1.5rem
-    line-height 2rem
+    font-size 1.2rem
+    color #fff
+    background-color $accentColor
+    padding 0.8rem 1.6rem
+    border-radius 4px
+    transition background-color .1s ease
+    box-sizing border-box
+    border-bottom 1px solid darken($accentColor, 10%)
+    margin 16px
+    &:hover
+      background-color lighten($accentColor, 10%)
     &:first-child
       margin-left 0
   .repo-link
