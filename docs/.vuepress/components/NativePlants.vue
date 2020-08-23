bb09ac3da9e3be0b0fe7f6ed5a3aa4fb20ddb714
Edward Shepard <eshepard81@gmail.com>
2020-08-22 20:53:38 -0500

    Make NativePlants filterable

---
+++ docs/.vuepress/components/NativePlants.vue
@@ -0,0 +1,150 @@
+<template>
+<div class="plants-container">
+    <div
+        v-for="plant in filteredPlants"
+        :key="`plant-${plant.id}`"
+        :style="plantBg(plant)"
+        class="plant"
+    >
+        <div class="eco-benefits">
+            <div>
+                <img v-for="(iconSrc, idx) in ecoIcons(plant)" :key="`ico-${idx}`" :src="iconSrc">
+            </div>
+        </div>
+        <img :alt="`A photo of ${plant.scientificName}`" :src="plant.imageUrl">
+        <div class="plant-info">
+        <ul>
+            <li class="plant-name">{{ plant.commonName }} ({{ plant.scientificName }})</li>
+            <li class="plant-stat"><img class="icon" src="/img/sun.svg"> {{ plant.lighting }}</li>
+            <li class="plant-stat"><img class="icon" src="/img/water.svg"> {{ plant.water }}</li>
+        </ul>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import plantData from '../../ar-native-plants.js';
+
+export default {
+  name: 'NativePlants',
+  props: {
+    season: String,
+    lighting: String,
+    water: String,
+  },
+  data() {
+    return {
+        plants: plantData,
+    };
+  },
+  computed: {
+    filteredPlants() {
+        let result;
+        if (this.season) {
+            result = this.plants.filter(p => p.bloomPeriod === this.season);
+        }
+        if (this.lighting) {
+            result = result.filter(p => p.lighting === this.lighting);
+        }
+        if (this.water) {
+            result = result.filter(p => p.water === this.water);
+        }
+        return result;
+    },
+    plantsBySeason() {
+        let spring = this.plants.filter(p => p.bloomPeriod === 'Spring');
+        let springSummer = this.plants.filter(p => p.bloomPeriod === 'Spring to Summer');
+        let summer = this.plants.filter(p => p.bloomPeriod === 'Summer');
+        let summerFall = this.plants.filter(p => p.bloomPeriod === 'Summer to Fall');
+        let fall = this.plants.filter(p => p.bloomPeriod === 'Fall');
+        let result;
+        switch (this.season) {
+            case 'Spring': result = spring; break;
+            case 'Spring to Summer': result = springSummer; break;
+            case 'Summer': result = summer; break;
+            case 'Summer to Fall': result = summerFall; break;
+            case 'Fall': result = fall; break;
+            default: result = [
+                    ...spring,
+                    ...springSummer,
+                    ...summer,
+                    ...summerFall,
+                    ...fall,
+                ];
+        }
+        return result;
+    },
+  },
+  methods: {
+      plantBg(plant) {
+          switch (plant.bloomPeriod) {
+            case 'Spring':
+                return { backgroundColor: '#b4d66a' }
+            case 'Spring to Summer':
+                return { backgroundColor: '#7bb05c' }
+            case 'Summer':
+                return { backgroundColor: '#eacc44' }
+            case 'Summer to Fall':
+                return { backgroundColor: '#f5822c' }
+            case 'Fall':
+                return { backgroundColor: '#bf522a' }
+          }
+      },
+      ecoIcons(plant) {
+          return plant.benefits.split(', ')
+            .map((b) => {
+                switch (b) {
+                    case 'Bees': return '/img/bee.svg';
+                    case 'Butterflies': return '/img/butterfly.svg';
+                    case 'Moths': return '/img/moth.svg';
+                    case 'Hummingbirds': return '/img/hummingbird.svg';
+                }
+            })
+      },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+    .plants-container
+        width 100%
+        display flex
+        flex-flow row wrap
+        .plant
+            margin 6px
+            padding 8px
+            border-radius: 6px
+            flex-basis 20%
+            display flex
+            flex-flow column nowrap
+            justify-content flex-start
+            img
+                max-width 100%
+                border-radius 8px
+            .plant-info
+                padding 12px 0
+                ul
+                    list-style none
+                    list-style-type none
+                    margin 0
+                    padding 0
+                    li
+                        font-size 0.8rem
+                        display flex
+                        img
+                            width 16px
+                            margin-right 8px
+                    li.plant-name
+                        font-weight: bold;
+                    li.plant-stat
+                        /* padding-top 4px */
+                        line-height 24px
+                        font-size 0.6rem
+        .eco-benefits
+            display flex
+            justify-content space-between
+            img
+                width 24px
+                margin 4px
+</style>
--- docs/native-plants.md
+++ docs/native-plants.md
@@ -1,109 +1,20 @@
 # Arkansas Native Plants
 
-<template>
-<div class="plants-container">
-    <div
-        v-for="plant, idx in plants"
-        :key="`plant-${plant.id}`"
-        :style="plantBg(plant)"
-        class="plant"
-    >
-        <div class="eco-benefits">
-            <div>
-                <img v-for="iconSrc in ecoIcons(plant)" :src="iconSrc">
-            </div>
-        </div>
-        <img :alt="`A photo of ${plant.scientificName}`" :src="plant.imageUrl">
-        <div class="plant-info">
-        <ul>
-            <li class="plant-name">{{ plant.commonName }} ({{ plant.scientificName }})</li>
-            <li class="plant-stat"><img class="icon" src="/img/sun.svg"> {{ plant.lighting }}</li>
-            <li class="plant-stat"><img class="icon" src="/img/water.svg"> {{ plant.water }}</li>
-        </ul>
-        </div>
-    </div>
-</div>
-</template>
+## Spring
+<native-plants
+    season="Spring"
+    lighting="Sun to Medium Shade"
+    water="Medium"
+/>
 
-<script>
-import plantData from './ar-native-plants.js';
-export default {
-  name: 'NativePlants',
-  data() {
-    return {
-        plants: plantData,
-    };
-  },
-  methods: {
-      plantBg(plant) {
-          switch (plant.bloomPeriod) {
-            case 'Spring':
-                return { backgroundColor: '#b4d66a' }
-            case 'Spring to Summer':
-                return { backgroundColor: '#7bb05c' }
-            case 'Summer':
-                return { backgroundColor: '#eacc44' }
-            case 'Summer to Fall':
-                return { backgroundColor: '#f5822c' }
-            case 'Fall':
-                return { backgroundColor: '#bf522a' }
-          }
-      },
-      ecoIcons(plant) {
-          return plant.benefits.split(', ')
-            .map((b) => {
-                switch (b) {
-                    case 'Bees': return '/img/bee.svg';
-                    case 'Butterflies': return '/img/butterfly.svg';
-                    case 'Moths': return '/img/moth.svg';
-                    case 'Hummingbirds': return '/img/hummingbird.svg';
-                }
-            })
-      },
-  },
-};
-</script>
+## Spring to Summer
+<native-plants season="Spring to Summer" />
 
-<style lang="stylus" scoped>
-    .plants-container
-        width 100%
-        display flex
-        flex-flow row wrap
-        .plant
-            margin 6px
-            padding 8px
-            border-radius: 6px
-            min-width 200px
-            max-width 200px
-            display flex
-            flex-flow column nowrap
-            justify-content flex-start
-            img
-                max-width 100%
-                border-radius 8px
-            .plant-info
-                padding 12px 0
-                ul
-                    list-style none
-                    list-style-type none
-                    margin 0
-                    padding 0
-                    li
-                        font-size 0.8rem
-                        display flex
-                        img
-                            width 16px
-                            margin-right 8px
-                    li.plant-name
-                        font-weight: bold;
-                    li.plant-stat
-                        /* padding-top 4px */
-                        line-height 24px
-                        font-size 0.6rem
-        .eco-benefits
-            display flex
-            justify-content space-between
-            img
-                width 24px
-                margin 4px
-</style>
+## Summer
+<native-plants season="Summer" />
+
+## Summer to Fall
+<native-plants season="Summer to Fall" />
+
+## Fall
+<native-plants season="Fall" />
