5c2efd015a4a9ea68b79c5051a11eaacbabb3077
edwardcxyz <eshepard81@gmail.com>
2020-11-16 23:14:08 -0600

    favicon!

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,29 @@ module.exports = {
         ['link', {
             rel: 'stylesheet',
             href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap',
-        }]
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/apple-touch-icon.png',
+        }],
+        ['link', {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/favicon-32x32.png',
+        }],
+        ['link', {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/favicon-16x16.png',
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/site.manifest',
+        }],
+        
     ],
     themeConfig: {
         logo: '/img/logo.png',
