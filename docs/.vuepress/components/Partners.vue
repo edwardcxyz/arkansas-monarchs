c00c82be78197b2ecdb97e09ba164309e7114de7
Edward <eshepard81@gmail.com>
2020-12-19 21:37:44 -0600

    start implementing feature see-more for partner description

--- docs/.vuepress/components/Partners.vue
+++ docs/.vuepress/components/Partners.vue
@@ -8,21 +8,23 @@
       <div class="partner-image">
         <img :src="partner.image" :alt="partner.name" />
       </div>
-      <div class="partner-description">
-        <p>{{ partner.about }}</p>
-        <p v-if="partner.about2">{{ partner.about2 }}</p>
-        <div class="social-icons">
+      <div class="partner-info">
+        <div class="description">
+          <p>{{ partner.about }}</p>
+          <p>{{ partner.about2 }}</p>
+          <div v-if="partner.about2" class="see-more">See more...</div>
+        </div>
+        <div class="social">
           <a v-if="partner.facebook" :href="partner.facebook">
-            <img src="/img/facebook.png" alt="facebook icon" />
+            <img src="/img/facebook.png" alt="facebook logo icon" />
           </a>
           <a v-if="partner.instagram" :href="partner.instagram">
-            <img src="/img/instagram.png" alt="instagram icon" />
+            <img src="/img/instagram.png" alt="instagram logo icon" />
           </a>
           <a v-if="partner.twitter" :href="partner.twitter">
-            <img src="/img/twitter.png" alt="twitter icon" />
+            <img src="/img/twitter.png" alt="twitter logo icon" />
           </a>
         </div>
-
       </div>
     </div>
   </div>
@@ -45,40 +47,39 @@ export default {
     background $fff
     box-shadow 8px 8px 24px 1px $body-bg
     display flex
+    flex-flow row wrap
     margin 24px
     padding 24px
-  .partner-image
-    width 40%
-    display flex
-    flex-direction column
-    justify-content center
-    align-items center
-    img
-      display block
-      object-fit contain
-      width 90%
-      height 200px
-    .social-icons
+    .partner-image
+      width 40%
       display flex
-      margin 24px 0 0 0
       img
-        width 32px
-        height 32px
-        margin 0 12px
-  .partner-description
-    display flex
-    flex-direction column
-    align-items center
-    width 50%
-    padding 12px
-    .social-icons
-      display flex
-      margin 24px 0 0 0
-      width 100%
-      justify-content center
-      img
-        width 32px
-        height 32px
-        margin 0 12px
-
+        display block
+        object-fit contain
+        width 90%
+        height 200px
+    .partner-info
+      width 50%
+      .description
+        overflow hidden
+        position relative
+        .see-more
+          font-family Montserrat, sans-serif
+          background-image: linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,1));
+          width 100%
+          position absolute
+          bottom 0px
+          right 0
+          text-align right
+          padding-top 24px
+          color #999
+      .social
+        width 100%
+        margin-top 32px
+        display flex
+        justify-content center
+        img
+          height 24px
+          width 24px
+          margin 0 12px
 </style>
